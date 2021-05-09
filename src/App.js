d21be815ac4bbda46b2f3af261344c421794b932
Dennis Demir <dd222gc@student.lnu.se>
2021-05-09 16:12:07 +0200

    m

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ const App = () => {
       <>
         <Router>
           <Switch>
-            <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Home} />
           </Switch>
         </Router>
       </>
