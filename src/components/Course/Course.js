780141e4705b8aab6279b543e6ef6407045876f3
CrawlingMuffin <c_nygren@protonmail.com>
2021-05-12 22:29:58 +0200

    Can now iterate through reviews

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -9,11 +9,20 @@ const Course = (course) => {
                 <h1 className="text-2xl font-medium text-primary mt-4 mb-12 ">{courseInfo.course.courseTitle}</h1>
                 <h2 className="text-2xl font-medium text-primary mt-4 mb-12 ">{courseInfo.course.courseID}</h2>
             </div>
-            <div className="reviews">
-                
-                <div className="">
+            <div className="reviewBox">
 
-                </div>
+            </div>
+            <div className="reviews">
+                {courseInfo.review.courseReviews.map(review => {
+                    return (
+                        <div>
+                            <h1>{review.rating}</h1>
+                            <p className="description">
+                                {review.message}
+                            </p>
+                        </div>
+                    )
+                })}
             </div>
         </div>
         </>
