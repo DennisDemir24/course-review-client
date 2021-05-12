c0f0be543e9d3f6e8da9935190d083b3e6e69fb6
CrawlingMuffin <c_nygren@protonmail.com>
2021-05-12 23:55:37 +0200

    Trying styles of comments

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -3,7 +3,6 @@ import courseInfo from './info.json'
 
 const getTotalRating = (reviews) => {
     let score = 0
-    let amount = reviews.courseReviews.length
     reviews.courseReviews.map(review => {
         score += review.rating
     })
@@ -14,13 +13,20 @@ const getTotalRating = (reviews) => {
 const Course = (course) => {
 
 
+    //Component inspiration from https://tailwindcomponents.com/component/comments
     return (
         <>
         <div className="body">
+                <div class="m-3">
+                    <button class="bg-white text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                        <span class="mr-2">English</span>
+                    </button>
+                </div>
             <div className="courseInfo">
-                <h1>{getTotalRating(courseInfo.review)}</h1>
-                <h1 className="text-2xl font-medium text-primary mt-4 mb-12 ">{courseInfo.course.courseTitle}</h1>
-                <h2 className="text-2xl font-medium text-primary mt-4 mb-12 ">{courseInfo.course.courseID}</h2>
+                <h1 className="text-2xl font-medium text-primary mt-4 mb-6 ">{courseInfo.course.courseTitle}</h1>
+                <h2 className="text-2xl font-medium text-primary mt-4 mb-6 ">{courseInfo.course.courseID}</h2>
+                <h1 className="text-2xl font-medium text-primary mt-4 mb-6 ">{getTotalRating(courseInfo.review)}/5</h1>
+                <p className="mb-6">{courseInfo.course.prerequisites}</p>
             </div>
             <div className="reviewBox">
 
@@ -28,11 +34,15 @@ const Course = (course) => {
             <div className="reviews">
                 {courseInfo.review.courseReviews.map(review => {
                     return (
-                        <div>
-                            <h1>{review.rating}</h1>
-                            <p className="description">
-                                {review.message}
-                            </p>
+                            <div className="bg-white rounded-lg p-3  flex flex-col justify-center items-center md:items-start shadow-lg mb-4">
+                                <div className="flex flex-row justify-center mr-2">
+                                    <h3 className="text-purple-600 font-semibold text-lg text-center md:text-left ">{review.studentID}</h3>
+                                </div>
+
+
+                                <p className="text-gray-600 text-lg text-center md:text-left ">{review.message}</p>
+
+                            {(review.score.length ? <p>{review.score.length} Found this review helpful!</p> : null)} 
                         </div>
                     )
                 })}
