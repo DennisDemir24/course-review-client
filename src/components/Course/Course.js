b348a7fa558d079fd1357809956031682226e5bb
CrawlingMuffin <c_nygren@protonmail.com>
2021-05-12 21:56:50 +0200

    Trying to iterate through reviews

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -4,8 +4,18 @@ import courseInfo from './info.json'
 const Course = (course) => {
     return (
         <>
-            <h1>{courseInfo.course.courseTitle}</h1>
-            <h2>{courseInfo.course.courseID}</h2>
+        <div className="body">
+            <div className="courseInfo">
+                <h1 className="text-2xl font-medium text-primary mt-4 mb-12 ">{courseInfo.course.courseTitle}</h1>
+                <h2 className="text-2xl font-medium text-primary mt-4 mb-12 ">{courseInfo.course.courseID}</h2>
+            </div>
+            <div className="reviews">
+                
+                <div className="">
+
+                </div>
+            </div>
+        </div>
         </>
     )
 }
