8e8d91940737d3049d177ab003208d46ea4df7a7
CrawlingMuffin <c_nygren@protonmail.com>
2021-05-12 22:43:57 +0200

    Average score now visible

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import courseInfo from './info.json'
 
+const getTotalRating = (reviews) => {
+    let score = 0
+    let amount = reviews.courseReviews.length
+    reviews.courseReviews.map(review => {
+        score += review.rating
+    })
+    let total = score / reviews.courseReviews.length
+    return total
+}
+
 const Course = (course) => {
+
+
     return (
         <>
         <div className="body">
             <div className="courseInfo">
+                <h1>{getTotalRating(courseInfo.review)}</h1>
                 <h1 className="text-2xl font-medium text-primary mt-4 mb-12 ">{courseInfo.course.courseTitle}</h1>
                 <h2 className="text-2xl font-medium text-primary mt-4 mb-12 ">{courseInfo.course.courseID}</h2>
             </div>
