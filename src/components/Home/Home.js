b7a41c3296cd820b06f495f8a5864f2806909e88
Dennis Demir <dd222gc@student.lnu.se>
2021-05-13 21:33:18 +0200

    search course functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import SearchBar from '../ui/SearchBar'
+import { connect } from 'react-redux'
+import { searchCourses } from '../../actions/courseActions'
 
-const Home = () => {
+const Home = ({ course: {courses}}) => {
+
+    useEffect(() => {
+
+    }, [])
+
     return (
-        <>
-            Home Screen
-        </>
+      <div className="bg-gray-900">
+        <div className="text-white">NAVBAR</div>
+        <SearchBar />
+        {courses !== null && courses.map((course) => {
+          return (
+            <div key={course.courseID}>
+              <h5 className="text-white">{course.courseID}</h5>
+              <h5 className="text-white">{course.courseTitle}</h5>
+            </div>
+          )
+        })}
+      </div>
     )
 }
 
-export default Home
+const mapStateToProps = (state) => ({
+  course: state.course,
+})
+
+export default connect(mapStateToProps, { })(Home)
