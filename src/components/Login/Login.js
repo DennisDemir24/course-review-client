0b0f963f2e3337164dfc49bdd94998ec12089861
mWalrus <hellkvistoskar@protonmail.com>
2021-05-15 13:56:44 +0200

    fixing styling issues for login button in header

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -39,7 +39,7 @@ const Login = ({ login, auth: { isAuthenticated }, history }) => {
       <>
         <button
           onClick={onToggleVisible}
-          className="absolute right-10 top-6 bg-yellow-400 rounded p-2 px-9 hover:bg-gray-600 hover:text-white text-black"
+          className="absolute right-10 top-6 bg-yellow-400 focus:outline-none rounded p-2 px-9 hover:bg-gray-600 hover:text-white text-black"
         >Login</button>
         <div className={
           visible ?
