1ee251d32761674c2b099290af82796dff2e1178
mWalrus <hellkvistoskar@protonmail.com>
2021-05-19 22:08:34 +0200

    Adding global navigation to the header. click title to return to home

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -4,7 +4,9 @@ import Login from '../Login/Login'
 const Header = () => {
   return (
     <div className="bg-jet-black-header text-white font-general font-medium border-yellow-400 border-b-4 p-4">
-      <h1 className="font-bold font-title text-3xl">Kurskollen</h1>
+      <h1 className="font-bold font-title text-3xl">
+        <a href="/">Kurskollen</a>
+      </h1>
       <p>Se vad studenter tycker om dina framtida kurser</p>
       <Login/>
     </div>
